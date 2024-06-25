d12c1d178a44db6a259480d51e60a3973008de9f
injelika <angelicasantos040604@gmail.com>
2024-06-25 23:18:16 +0800

    HI

---
+++ app/src/main/java/com/example/santos_act5/MainActivity.kt
@@ -0,0 +1,186 @@
+package com.example.SANTOS_ACT5
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.VisibleForTesting
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.santos_act5.R
+import com.example.santos_act5.ui.theme.SANTOS_ACT5Theme
+import java.text.NumberFormat
+import kotlin.math.ceil
+import androidx.compose.material3.Surface as Surface1
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SANTOS_ACT5Theme {
+                Surface1(
+                    modifier = Modifier.fillMaxSize(),
+                    color = colorScheme.background
+                ) {
+                    TipTimeLayout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TipTimeLayout() {
+
+    // For the amount Input so the program can remember its state and values
+    var amountInput by remember { mutableStateOf("")}
+    // Converting the amount Input to double or if it's not convertable into a Double it will become null, but since the use of "?:"
+    // elvis operator null changes into 0.0
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+
+    var tipInput by remember { mutableStateOf("")}
+    val tipPercentage = tipInput.toDoubleOrNull() ?: 0.0
+
+    var roundUp by remember { mutableStateOf(false)}
+
+    val tip = calculateTip(amount, tipPercentage, roundUp)
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp)
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.calculate_tip),
+            modifier = Modifier
+                .padding(bottom = 16.dp, top = 40.dp)
+                .align(alignment = Alignment.Start)
+        )
+
+        EditNumberField(amountInput,
+            R.string.bill_amount,
+            KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            onValueChange = { amountInput = it},
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth())
+
+        EditNumberField(tipInput,
+            R.string.how_was_the_service,
+            KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            onValueChange = { tipInput = it},
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth())
+
+        // calling the function RoundTheTipRow and supplementing the required parameters for it
+        RoundTheTipRow(roundUp,
+            onRoundUpChanged = { roundUp = it },
+            modifier = Modifier.padding(bottom = 32.dp))
+
+        Text(
+            text = stringResource(R.string.tip_amount, tip),
+            style = MaterialTheme.typography.displaySmall
+        )
+
+        // To add space :|
+        Spacer(modifier = Modifier.height(150.dp))
+    }
+}
+
+@VisibleForTesting
+fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+
+    if(roundUp){
+        // always round up
+        tip = ceil(tip)
+    }
+
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+// For Text Input from the user
+@Composable
+fun EditNumberField(amountInput:String,
+                    label: Int,
+                    keyboardOptions: KeyboardOptions,
+                    onValueChange: (String) -> Unit,
+                    modifier: Modifier = Modifier) {
+
+    TextField(
+        value = amountInput,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        singleLine = false,
+        keyboardOptions = keyboardOptions,
+        label = { Text(stringResource(label)) },
+    )
+}
+
+// Function for the Round up or as is button
+@Composable
+fun RoundTheTipRow(roundUp: Boolean, onRoundUpChanged: (Boolean) -> Unit, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(R.string.round_up_tip))
+
+        // The calling of button
+        Switch(
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged,
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End)
+        )
+    }
+}
+
+
+// To show the Preview of the code usually seen at the Split tab
+@Preview (showBackground = true)
+@Composable
+fun Preview(){
+    TipTimeLayout()
+}
